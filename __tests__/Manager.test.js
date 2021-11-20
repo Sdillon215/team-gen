c5e74e889747e0b4c7d4a5b57c1a5a4c8dc20281
Sean <sdillon215@gmail.com>
2021-11-20 03:31:20 -0800

    test fix

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,7 +7,7 @@ test('role', () => {
 });
 
 test('office number', () => {
-    const manager = new Manager('Joe', 2, 'Blah@bleh.com', 20);
+    const manager = new Manager('Bob', 2, 'bob@email.com', 20);
 
-    expect(manager.getOfficeN()).toBe(20);
+    expect(manager.getOfficeNum()).toBe(20);
 });
